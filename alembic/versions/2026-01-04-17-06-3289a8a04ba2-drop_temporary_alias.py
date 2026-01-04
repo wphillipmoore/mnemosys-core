e515592f10f3515a0929c2cad1e34f630e774445
Phillip Moore <w.phillip.moore@gmail.com>
2026-01-04 17:15:17 -0500

    Sort migration imports

--- alembic/versions/2026-01-04-17-06-3289a8a04ba2-drop_temporary_alias.py
+++ alembic/versions/2026-01-04-17-06-3289a8a04ba2-drop_temporary_alias.py
@@ -5,9 +5,8 @@ Revises: cd75eb275cb7
 Create Date: 2026-01-04 17:06:57.446739
 
 """
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "3289a8a04ba2"
